084116c3c89f82363fd439facef5947e4f142d7f
Unknown <jiefeis2@illinois.edu>
2017-11-21 13:09:31 -0600

    first version home page

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,9 +5,9 @@
         .module('hopefullyApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'Course', 'Pic'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController ($scope, Principal, LoginService, $state, Course, Pic) {
         var vm = this;
 
         vm.account = null;
@@ -19,6 +19,7 @@
         });
 
         getAccount();
+        loadAll();
 
         function getAccount() {
             Principal.identity().then(function(account) {
@@ -28,6 +29,45 @@
         }
         function register () {
             $state.go('register');
+        }
+        function loadAll() {
+            Course.getall().query(function(result) {
+                vm.courses = result;
+                vm.searchQuery = null;
+                vm.len = vm.courses.length;
+
+                vm.idx = new Array(vm.courses[vm.courses.length - 1].id+1);
+                for (var i = 0; i < vm.courses.length; i ++) {
+                    vm.idx[vm.courses[i].id] = i;
+                    vm.courses[i].picture = [];
+                    vm.courses[i].comment = [];
+
+                }
+
+                Pic.getpic().query(function (result) {
+                    vm.pics = result;
+                    for(var i = 0; i < vm.pics.length; i ++) {
+                        vm.courses[vm.idx[vm.pics[i].coursepic.id]].picture.push(vm.pics[i]);
+                    }
+                     vm.tmp = vm.courses[0];
+
+                    // vm.len = vm.courses[0].name;
+                });
+              //  vm.tmp = vm.courses[0].picture.length;
+
+                // Comment.getcomment().query(function (result) {
+                //     vm.comments = result;
+                //     for (var i = 0; i < vm.comments.length; i ++) {
+                //         vm.courses[vm.idx[vm.comments[i].targetcourse.id]].comment.push(vm.comments[i]);
+                //     }
+                //     //vm.courses[2].comment is an array
+                //     vm.tmp = vm.courses[2].comment[0].review;
+                // })
+
+
+
+            });
+
         }
     }
 })();
